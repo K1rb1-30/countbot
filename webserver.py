53f47d2095d690ad136dca6ce20f31c56db5343e
Gabriel Sánchez Flores <f1resecundario@gmail.com>
2025-11-14 16:48:30 +0100

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -8,8 +8,8 @@ def index():
     return "Bot is running"
 
 def run():
-    app.run(host='0.0.0.0', port=8000)
-    
+    app.run(host='0.0.0.0', port=8080)
+
 def keep_alive():
     server = Thread(target=run)
     server.start()
